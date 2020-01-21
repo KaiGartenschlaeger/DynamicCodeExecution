a45add47abc825cd56dd385eda06043b93ffc1f9
KaiGartenschlaeger <kai.gartenschlaeger@awin.com>
2020-01-21 15:08:44 +0100

    Add project files.

---
+++ DynamicCodeExecution/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+
+namespace DynamicCodeExecution
+{
+    class Program
+    {
+        static CSharpCompilation GenerateCode(string sourceCode)
+        {
+            var codeString = SourceText.From(sourceCode);
+            var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp7_3);
+
+            var parsedSyntaxTree = SyntaxFactory.ParseSyntaxTree(codeString, options);
+
+            var references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Console).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(System.Runtime.AssemblyTargetedPatchBandAttribute).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Microsoft.CSharp.RuntimeBinder.CSharpArgumentInfo).Assembly.Location),
+            };
+
+            return CSharpCompilation.Create("DynamicExecute.dll",
+                new[] { parsedSyntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.ConsoleApplication,
+                    optimizationLevel: OptimizationLevel.Release,
+                    assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default));
+        }
+
+        static void Main(string[] args)
+        {
+            using (var peStream = new MemoryStream())
+            {
+                var sourceCode = File.ReadAllText("Test.cs");
+                var result = GenerateCode(sourceCode).Emit(peStream);
+                if (!result.Success)
+                {
+                    Console.WriteLine("Compilation done with error.");
+
+                    var failures = result.Diagnostics
+                        .Where(diagnostic => diagnostic.IsWarningAsError || diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (var diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+
+                    //return null;
+                }
+
+                Console.WriteLine("Compilation done without any error.");
+                peStream.Seek(0, SeekOrigin.Begin);
+
+                //return peStream.ToArray();
+            }
+        }
+    }
+}
---
+++ DynamicCodeExecution/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicCodeExecution
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test 123");
+        }
+    }
+}
